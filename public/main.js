803a92b88403f34a7716e563a3829416add84144
noavish <noavish1@gmail.com>
2018-01-21 11:25:54 +0200

    drag and drop

--- public/main.js
+++ public/main.js
@@ -1,8 +1,10 @@
+// SpaceBook module
 var SpacebookApp = function() {
 
     var posts = [];
     var $posts = $(".posts");
 
+    // Getting all the posts and comments from the DB
     var fetch = function () {
         $.ajax({
             method: "GET",
@@ -12,14 +14,18 @@ var SpacebookApp = function() {
                 console.log(data);
                 posts = data;
                 _renderPosts();
+                $('[data-toggle="popover"]').popover();
+                $('.comments-container').toggleClass('show');
             }, error: function (jqXHR, testStatus) {
                 console.log(testStatus);
             }
         });
     };
 
+    // Calling fetch
     fetch();
 
+    // Update the posts view
     function _renderPosts() {
         console.log(posts.length);
         $posts.empty();
@@ -33,28 +39,10 @@ var SpacebookApp = function() {
         }
     }
 
-    function addPost(newPost) {
-        $.ajax({
-            type: "POST",
-            url: 'posts',
-            data: {postText: newPost},
-            success: function () {
-                console.log('server returned a success response (HTTP response with status 200)');
-            },
-            error: function (jqXHR, testStatus) {
-                console.log(testStatus);
-            },
-            dataType: 'json'
-        });
-        // posts.push({text: newPost, comments: [] });
-        // _renderPosts();
-        fetch();
-    }
-
-
+    // Update the comments view per post according to postID
     function _renderComments(postIndex) {
         var post = $(".post")[postIndex];
-        $commentsList = $(post).find('.comments-list')
+        $commentsList = $(post).find('.comments-list');
         $commentsList.empty();
         var source = $('#comment-template').html();
         var template = Handlebars.compile(source);
@@ -64,102 +52,230 @@ var SpacebookApp = function() {
         }
     }
 
-        var removePost = function(id, postIndex) {
-            $.ajax({
-                type: "DELETE",
-                url: `posts/${id}`,
-                success: function () {
-                    console.log('Delete');
-                    posts.splice(postIndex, 1);
-                    _renderPosts();
-                },
-                error: function (jqXHR, testStatus) {
-                    console.log(testStatus);
-                }
-            });
-        };
+    //Get new post text and add a new post object to the DB and to the view
+    function addPost(newPost) {
+        $.ajax({
+            type: "POST",
+            url: 'posts',
+            data: {postText: newPost},
+            success: function (data) {
+                console.log('server returned a success response (HTTP response with status 200)');
+                posts.push(data);
+                _renderPosts();
+            },
+            error: function (jqXHR, testStatus) {
+                console.log(testStatus);
+            },
+            dataType: 'json'
+        });
+    }
 
-        var addComment = function(newComment, id, postIndex) {
-            console.log(newComment);
-            $.ajax({
-                type: "POST",
-                url: `posts/${id}/comments`,
-                data: newComment,
-                success: function (data) {
-                    console.log(data);
-                    posts[postIndex].comments.push(newComment);
-                    _renderComments();
-                },
-                error: function (jqXHR, testStatus) {
-                    console.log(testStatus);
-                },
-                dataType: 'json'
-            });
-        };
-
-
-        var deleteComment = function(postIndex, commentIndex) {
-            posts[postIndex].comments.splice(commentIndex, 1);
-            _renderComments(postIndex);
-        };
-
-        return {
-            addPost: addPost,
-            removePost: removePost,
-            addComment: addComment,
-            deleteComment: deleteComment,
-        };
+    //Get postIndex and postID and remove the post object from the DB and from the view
+    var removePost = function(id, postIndex) {
+        $.ajax({
+            type: "DELETE",
+            url: `posts/${id}`,
+            success: function () {
+                console.log('Delete');
+                posts.splice(postIndex, 1);
+                _renderPosts();
+            },
+            error: function (jqXHR, testStatus) {
+                console.log(testStatus);
+            }
+        });
     };
+
+    //Get a newComment object, postID and postIndex and add a new comment to the correct post in the DB and the view
+    var addComment = function(newComment, id, postIndex) {
+        console.log(newComment);
+        $.ajax({
+            type: "POST",
+            url: `posts/${id}/comments/newComment`,
+            data: newComment,
+            success: function (data) {
+                console.log(data);
+                newComment._id = data.newCommentId;
+                posts[postIndex].comments.push(newComment);
+                _renderComments(postIndex);
+            },
+            error: function (jqXHR, testStatus) {
+                console.log(testStatus);
+            },
+            dataType: 'json'
+        });
+    };
+
+    // Get postID and postIndex and commentId and commentIndex and remove the right comment from the right post in DB and view
+    var deleteComment = function(commentID, postID, postIndex, commentIndex) {
+        $.ajax({
+            type: "DELETE",
+            url: `posts/${postID}/comments/removeComment`,
+            data: {commentID: commentID},
+            success: function () {
+                console.log('Delete');
+                posts[postIndex].comments.splice(commentIndex, 1);
+                _renderComments(postIndex);
+            },
+            error: function (jqXHR, testStatus) {
+                console.log(testStatus);
+            }
+        });
+    };
+
+    var findPostID = function (clickedElement) {
+        return clickedElement.closest('.post').data().id;
+    };
+
+    var updatePost = function (postID, postIndex, newText) {
+        $.ajax({
+            type: "PUT",
+            url: `posts/${postID}/updatePost`,
+            data: {
+                postText:newText
+            },
+            success: function (data) {
+                console.log(data);
+                console.log(posts);
+                console.log(postIndex);
+                posts[postIndex].postText = data;
+                _renderPosts();
+            },
+            error: function (jqXHR, testStatus) {
+                console.log(testStatus);
+            }
+        });
+    };
+
+    var changeCommentsOrder = function (postID, newIndex, oldIndex) {
+        $.ajax({
+            type: "PUT",
+            url: `posts/${postID}/comments/sort`,
+            data: {
+                newIndex: newIndex,
+                oldIndex: oldIndex
+            },
+            success: function (data) {
+                console.log(data);
+                posts[postIndex].comments = data;
+                _renderComments();
+            },
+            error: function (jqXHR, testStatus) {
+                console.log(testStatus);
+            }
+        })
+    };
+
+    //return functions out of the module
+    return {
+        addPost: addPost,
+        removePost: removePost,
+        addComment: addComment,
+        deleteComment: deleteComment,
+        findPostID: findPostID,
+        updatePost, updatePost,
+        changeCommentsOrder: changeCommentsOrder
+    };
+};
 
 var $posts = $(".posts");
 var app = SpacebookApp();
 
-
+// Click the addPost - calling the addPost function and clear the fields
 $('#addpost').on('click', function() {
-      var $input = $("#postText");
-      if ($input.val() === "") {
-        alert("Please enter text!");
-      } else {
-        app.addPost($input.val());
-        $input.val("");
-      }
+    debugger
+    var $input = $("#postText");
+    if ($input.val() === "") {
+    alert("Please enter text!");
+    } else {
+    app.addPost($input.val());
+    $input.val("");
+    }
 });
 
+// Click the removePost - calling the removePost function
 $posts.on('click', '.remove-post', function() {
-  var id = $(this).closest('.post').data().id;
-  var postIndex = $(this).closest('.post').index();
-  app.removePost(id, postIndex);
+    var id = app.findPostID($(this));
+    var postIndex = $(this).closest('.post').index();
+    app.removePost(id, postIndex);
 });
 
+// Click the Comments - toggling the comments list (show/hide)
 $posts.on('click', '.toggle-comments', function() {
-  var $clickedPost = $(this).closest('.post');
-  $clickedPost.find('.comments-container').toggleClass('show');
+    var $clickedPost = $(this).closest('.post');
+    $clickedPost.find('.comments-container').toggleClass('show');
 });
 
+// Click the addComment - calling the addComment function and clear the fields
 $posts.on('click', '.add-comment', function() {
-  var id = $(this).parents('.post').data().id;
-  var $comment = $(this).siblings('.comment');
-  var $user = $(this).siblings('.name');
+    var id = app.findPostID($(this));
+    var $comment = $(this).siblings('.comment');
+    var $user = $(this).siblings('.name');
 
-  if ($comment.val() === "" || $user.val() === "") {
+    if ($comment.val() === "" || $user.val() === "") {
     alert("Please enter your name and a comment!");
     return;
-  }
+    }
+    var postIndex = $(this).parents('.post').index();
+    var newComment = { comment: $comment.val(), userName: $user.val() };
 
-  var postIndex = $(this).closest('.post').index();
-  var newComment = { text: $comment.val(), user: $user.val() };
+    app.addComment(newComment, id, postIndex);
 
-  app.addComment(newComment, id, postIndex);
-
-  $comment.val("");
-  $user.val("");
-
+    $comment.val("");
+    $user.val("");
 });
 
+// Click the removeComment - calling the deleteComment function
 $posts.on('click', '.remove-comment', function() {
-  var $commentsList = $(this).closest('.post').find('.comments-list');
-  var postIndex = $(this).closest('.post').index();
-  var commentIndex = $(this).closest('.comment').index();
+    var postID = app.findPostID($(this));
+    var commentID = $(this).closest('li').data().id;
+    var postIndex = $(this).closest('.post').index();
+    var commentIndex = $(this).closest('.comment').index();
 
-  app.deleteComment(postIndex, commentIndex);
+    app.deleteComment(commentID, postID, postIndex, commentIndex);
+});
+
+
+// Click to updatePost - calling the updatePost function
+$posts.on('click', '.update-post', function () {
+    // var postID = app.findPostID($(this));
+    var postText = $(this).parents('.post').children('span').text();
+    $(this).parents('.post').children('span').html(`<input type='text' class='change-text' value=${postText}>`);
+});
+
+$posts.on('keyup', '.change-text', function(event) {
+    if (event.keyCode == 13 || event.which == 13) {
+        var postID = app.findPostID($(this));
+        var postIndex = $(this).parents('.post').index();
+        var newText = $(this).val();
+        $(this).parents('.post').html(`<span>${newText}</span>`);
+        app.updatePost(postID, postIndex, newText);
+    }
+});
+
+var oldIndex = 0;
+// $('.posts').on('dragstart', '.comment-li', function () {
+//     oldIndex = $(this).index();
+// });
+//
+// $('.posts').on('dragend', '.comment-li', function () {
+//    var newIndex = $(this).index();
+//
+//    }
+// });
+
+$(function() {
+    $( "#sortable" ).sortable({
+        start: function (err, ui) {
+            oldIndex = ui.item.index();
+        },
+        update: function (event, ui) {
+            var postID = app.findPostID($(this));
+            var newIndex = ui.item.index();
+            if (newIndex !== oldIndex) {
+                app.changeCommentsOrder(postID, newIndex, oldIndex);
+            }
+        }
+    });
+    $( "#sortable" ).disableSelection();
 });
--- server.js
+++ server.js
@@ -39,8 +39,13 @@ app.post('/posts', function (req, res) {
             postText: req.body.postText,
             comments: []
         });
-        post.save();
-        res.send({status: "ok", message: "Received."});
+        post.save(function(err , post){
+            if (err) {
+                throw err;
+            }
+            console.log(post);
+            res.send(post);
+        });
     } else{
         res.send({status: "nok", message: "Nothing received."});
     }
@@ -63,23 +68,22 @@ app.delete('/posts/:_id', function (req, res) {
     }
 });
 // 4) to handle adding a comment to a post
-app.post('/posts/:_id/comments', function (req, res) {
+app.post('/posts/:_id/comments/newComment', function (req, res) {
     if (req.body && req.params._id) {
         let comment = {
-            comment: req.body.text,
-            userName: req.body.user
+            comment: req.body.comment,
+            userName: req.body.userName
         };
         Post.findOne({_id: req.params._id}, function (err, post) {
             if (err) {
                throw err;
-            } else {}
-            post.comments.push(comment);
+            }
+            var commentsLength = post.comments.push(comment);
             post.save(function(err , data){
                 if (err) {
                     throw err;
                 }
-                res.send({status: "ok", message: data});
-
+                res.send({newCommentId: data.comments[commentsLength-1]._id});
             });
         });
     } else{
@@ -87,21 +91,57 @@ app.post('/posts/:_id/comments', function (req, res) {
     }
 });
 // 5) to handle deleting a comment from a post
-// app.delete('/posts/:_id/comments', function (req, res) {
-//     if (req.params._id) {
-//         console.log(req.params._id);
-//         Post.findByIdAndRemove(req.params._id, function (err) {
-//             if (err) {
-//                 console.error(err);
-//             } else {
-//                 console.log('post was deleted');
-//             }
-//         });
-//         res.send({status: "ok", message: "Deleted."});
-//     } else{
-//         res.send({status: "nok", message: "Nothing received."});
-//     }
-// });
+app.delete('/posts/:_id/comments/removeComment', function (req, res) {
+    if (req.params._id) {
+        console.log(req.params._id);
+        console.log(req.body.commentID);
+        Post.findOne({_id: req.params._id}).populate('comments').exec(function(err, post){
+            if (err) {
+                console.error(err);
+            } else {
+                post.comments.id(req.body.commentID).remove();
+                post.save();
+                console.log('comment was deleted');
+            }
+        });
+        res.send({status: "ok", message: "Deleted."});
+    } else{
+        res.send({status: "nok", message: "Nothing received."});
+    }
+});
+
+app.put('/posts/:_id/updatePost', function (req, res) {
+    if (req.params._id) {
+        console.log(req.params._id);
+        Post.findByIdAndUpdate(req.params._id, { $set: { postText: req.body.postText }}, { new: true },  function (err, post) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(post.postText);
+                res.send(post.postText);
+            }
+        });
+    } else{
+        res.send({status: "nok", message: "Nothing received."});
+    }
+});
+
+app.put('/posts/:_id/comments/sort', function (req, res) {
+    if (req.params._id) {
+        console.log(req.params._id);
+        Post.findById(req.params._id, function (err, post) {
+            if (err) {
+                console.error(err);
+            } else {
+                var temp = post.comments.splice(req.body.oldIndex, 1)[0];
+                post.comments.splice(req.body.newIndex, 0, temp);
+                post.save();
+            }
+        });
+    } else{
+        res.send({status: "nok", message: "Nothing received."});
+    }
+});
 
 app.listen(8000, function() {
   console.log("what do you want from me! get me on 8000 ;-)");
